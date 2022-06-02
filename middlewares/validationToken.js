6268438fda5f41214ea8b148628a1af7cbe6d6ac
Vitaliy Zavgorodniy <vince.kkr@gmail.com>
2022-06-02 08:29:13 +0300

    heroku changes

--- middlewares/validationToken.js
+++ middlewares/validationToken.js
@@ -1,7 +1,7 @@
 const createError = require('http-errors');
 const jwt = require('jsonwebtoken');
 
-const usersService = require('../domain/users/users.service');
+const { usersService } = require('../domain/users');
 
 const { SECRET_SALT } = process.env;
 const service = new usersService();
