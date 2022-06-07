97057d16b7a23ae490ae3def386da79a5a239a90
Vitaliy Zavgorodniy <vince.kkr@gmail.com>
2022-06-07 14:15:18 +0300

    add swagger book post library get

--- domain/libraries/librariesController.js
+++ domain/libraries/librariesController.js
@@ -3,10 +3,8 @@ const libaryService = require('./librariesService');
 const service = new libaryService();
 
 class librariesController {
-  addBookToLibrary = async (req, res) => {};
-
   removeBookFromLibrary = async (req, res) => {
-    await service.createBook(req.body);
+    // await service.createBook(req.body);
 
     res.status(200).json({ status: 200, message: 'ok' });
   };
