b7abd9b89394d308a59a168a520055027785b96a
Vitaliy Zavgorodniy <vince.kkr@gmail.com>
2022-06-14 20:59:23 +0300

    fixed pass length

--- domain/users/usersModel.js
+++ domain/users/usersModel.js
@@ -30,7 +30,7 @@ const userSchema = Schema(
 
 const registerSchema = Joi.object({
   name: Joi.string(),
-  password: Joi.string().min(6).required(),
+  password: Joi.string().min(5).required(),
   email: Joi.string().pattern(emailRegexp).required(),
 });
 
